8dbe741e52cc44258f6e914e4664d37f7c95df31
Guille De Fermin <guillermo.defermin@taxfix.de>
2021-03-05 15:09:12 +0100

    Add more test cases for styled-jss

--- test/styled-jss.tsx
+++ test/styled-jss.tsx
@@ -12,9 +12,57 @@ const RedBox = styled('div')({
   backgroundColor: 'red',
 });
 
+const BlueBox = styled('div')({
+  width: 100,
+  height: 100,
+  backgroundColor: 'blue',
+});
+
+const BlueAndRed = () => (
+  <div>
+    <RedBox />
+    <BlueBox />
+  </div>
+);
+
+const BlueAndRedFragment = () => (
+  <>
+    <RedBox />
+    <BlueBox />
+  </>
+);
+
+const BlueAndRedNesting = () => (
+  <div>
+    <RedBox />
+    <div>
+      <>
+        <RedBox />
+        <BlueBox />
+      </>
+      <BlueBox />
+    </div>
+  </div>
+);
+
 describe('styled-jss', () => {
   it('removes non-deterministic class names', () => {
     const redBox = render(<RedBox />).asFragment();
     expect(redBox).toMatchSnapshot();
+  });
+
+  it('can handle components with several styles', () => {
+    const blueRed = render(<BlueAndRed />).asFragment();
+    expect(blueRed).toMatchSnapshot();
+  });
+
+  it('can handle fragments', () => {
+    const blueRed = render(<BlueAndRedFragment />).asFragment();
+    expect(blueRed).toMatchSnapshot();
+  });
+
+  it('can handle components with nesting', () => {
+    const blueRed = render(<BlueAndRedNesting />).asFragment();
+    expect(blueRed).toMatchSnapshot();
   });
 });
